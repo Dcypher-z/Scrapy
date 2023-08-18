48bc78f32bd671fdc7f67af7907f5bb4ed75635f
Aryan Rastogi <rudranshrud@gmail.com>
2023-08-18 11:49:46 +0530

    pipelines

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -6,8 +6,68 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+from forex_python.converter import CurrencyRates
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+        
+        ## Strip all whitespaces from strings
+        adapter = ItemAdapter(item)
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                if value is not None:
+                    adapter[field_name] = value.strip()  
+        
+        ## Category & Product Type --> Switch to Lowercase
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            if value is not None:
+                adapter[lowercase_key] = value.lower()
+            
+        ## Price --> convert to float
+        price_keys = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            if value is not None:
+                value = value.replace('£', '')
+                print(value)
+                adapter[price_key] = CurrencyRates().convert("GBP", "INR", float(value))
+        
+        ##Availability --> extract number of books in stock
+        availability_string = adapter.get('availability')
+        split_string_array = availability_string.split('(')
+        if availability_string is not None:
+            if len(split_string_array) < 2:
+                adapter['avalability'] = 0
+            else:
+                availability_array = split_string_array[1].split(' ') 
+                adapter['availability'] = int(availability_array[0])     
+            
+            
+        ## Reviews --> convert string to int
+        num_reviews_string = adapter.get('num_reviews')
+        adapter['num_reviews'] = int(num_reviews_string)  
+        
+        ##star rating to integer
+        stars_string = adapter.get('stars')
+        split_stars_array = stars_string.split(' ')
+        star_text_value = split_stars_array[1].lower()
+        
+        if star_text_value == "zero":
+            adapter['stars'] = 0
+        elif star_text_value == "one":
+            adapter['stars'] = 1
+        elif star_text_value == "two":
+            adapter['stars'] = 2
+        elif star_text_value == "three":
+            adapter['stars'] = 3
+        elif star_text_value == "four":
+            adapter['stars'] = 4
+        elif star_text_value == "five":
+            adapter['stars'] = 5
+            
         return item
+ 
